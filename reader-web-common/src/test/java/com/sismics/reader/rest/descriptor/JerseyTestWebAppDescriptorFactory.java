7296af80a5c103051c838af32517633695c4d0eb
Jean-Marc Tremeaux <jm.tremeaux@gmail.com>
2013-04-17 12:28:16 +0200

    Set base directory in JerseyTest

--- reader-web-common/src/test/java/com/sismics/reader/rest/descriptor/JerseyTestWebAppDescriptorFactory.java
+++ reader-web-common/src/test/java/com/sismics/reader/rest/descriptor/JerseyTestWebAppDescriptorFactory.java
@@ -1,5 +1,7 @@
 package com.sismics.reader.rest.descriptor;
 
+import java.io.File;
+
 import com.sismics.util.filter.RequestContextFilter;
 import com.sismics.util.filter.TokenBasedSecurityFilter;
 import com.sun.jersey.test.framework.WebAppDescriptor;
@@ -10,12 +12,17 @@ import com.sun.jersey.test.framework.WebAppDescriptor;
  * @author jtremeaux
  */
 public class JerseyTestWebAppDescriptorFactory {
+    private static String basePath = new File("src/main/webapp").getAbsolutePath();
+    
     /**
      * Constructs a new descriptor.
      * 
      * @return Descriptor
      */
     public static WebAppDescriptor build() {
+        // Target the base path to the Webapp resources
+        System.setProperty("user.dir", basePath);
+        
         return new WebAppDescriptor.Builder("com.sismics.reader.rest.resource")
             .contextPath("reader")
             .addFilter(RequestContextFilter.class, "requestContextFilter")
