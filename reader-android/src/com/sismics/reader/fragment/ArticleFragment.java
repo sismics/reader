dd80af4e80c79624489f02c0ec2d373f72621898
jendib <benjamin.gam@gmail.com>
2013-06-16 21:08:47 +0200

    Android: article content fits in screen width

--- reader-android/src/com/sismics/reader/fragment/ArticleFragment.java
+++ reader-android/src/com/sismics/reader/fragment/ArticleFragment.java
@@ -1,5 +1,8 @@
 package com.sismics.reader.fragment;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,6 +40,7 @@ public class ArticleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.article_fragment, container, false);
         WebView webView = (WebView) view.findViewById(R.id.articleWebView);
+        webView.getSettings().setUseWideViewPort(true);
         
         Bundle args = getArguments();
         if (args != null) {
@@ -44,7 +48,28 @@ public class ArticleFragment extends Fragment {
             if (jsonStr != null) {
                 try {
                     JSONObject json = new JSONObject(jsonStr);
-                    webView.loadData(json.optString("description"), "text/html; charset=UTF-8", null);
+                    
+                    // HTML modification to fit the article content in the screen width
+                    String html = json.optString("description");
+                    try {
+                        html = "<!DOCTYPE html>" +
+                        		"<html>" +
+                        		"<head>" +
+                        		"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />" +
+                        		"<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width, maximum-scale=1, user-scalable=no\" />" +
+                        		"<style>" +
+                        		"img, iframe { max-width: 100%; height: auto; } " +
+                        		"pre { max-width: 100%; overflow: hidden; } " +
+                        		"</style>" +
+                        		"</head>" +
+                        		"<body>" +
+                        		URLEncoder.encode(html, "UTF-8").replaceAll("\\+", "%20") +
+                        		"</body>" +
+                        		"</html>";
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e("ArticleFragment", "Error modifying article HTML", e);
+                    }
+                    webView.loadData(html, "text/html; charset=UTF-8", null);
                 } catch (JSONException e) {
                     Log.e("ArticleFragment", "Unable to parse JSON", e);
                 }
