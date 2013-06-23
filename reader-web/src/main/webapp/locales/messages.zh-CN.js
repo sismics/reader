9ac57c9074c0f26eb2d748e6012eef015eda6dd2
jendib <benjamin.gam@gmail.com>
2013-06-23 13:27:10 +0200

    Chinese translation by Ryan H. Wang

---
+++ reader-web/src/main/webapp/locales/messages.zh-CN.js
@@ -0,0 +1,182 @@
+﻿{
+  "app": "Sismics Reader",
+  "error": {
+    "unknown": "载入遇到未知错误，请重试",
+    "feed": "加载条目遇到错误，请重试"
+  },
+  "login": {
+    "username": "用户名",
+    "password": "密码",
+    "remember": "记住登录信息",
+    "submit": "登录",
+    "error": "用户名或者密码错误"
+  },
+  "defaultpassword": {
+    "info": "请使用默认用户名和密码 \"admin\" 登录，建议登陆后立即修改密码",
+    "warning": "您仍未更改默认密码，请立即修改密码",
+    "dismiss": "知道了"
+  },
+  "subscription": {
+    "latest": "最新条目",
+    "unread": "未读条目",
+    "all": "所有条目",
+    "starred": "加星标的条目",
+    "subscriptions": "订阅",
+    "empty": "没有订阅项目",
+    "emptyunread": "没有未读条目",
+    "showall": "显示全部",
+    "toggleunread": "切换显示所有/未读条目",
+    "add": {
+      "button": "订阅",
+      "placeholder": "粘贴一个供稿链接或是网页地址",
+      "submit": "添加"
+    },
+    "edit": {
+      "button": "编辑",
+      "placeholder": "名称",
+      "submit": "确定",
+      "delete": "删除",
+      "deleteconfirm": "确认删除选定的条目？"
+    }
+  },
+  "category": {
+    "nonesting": "无法添加折叠分类",
+    "empty": "没有其他分类",
+    "add": {
+      "placeholder": "名称",
+      "submit": "添加",
+      "button": "添加一个分类"
+    },
+    "edit": {
+      "button": "编辑",
+      "placeholder": "名称",
+      "submit": "确定",
+      "delete": "删除",
+      "deleteconfirm": "确认删除选定的分类？"
+    }
+  },
+  "toolbar": {
+    "showall": "所有条目",
+    "shownew": "新条目",
+    "allread": "全部标为已读",
+    "category": "分类",
+    "back": "&larr; 返回",
+    "settings": "设定",
+    "about": "关于",
+    "logout": "退出"
+  },
+  "settings": {
+    "tabs": {
+      "account": "账户",
+      "importexport": "导入/导出",
+      "users": "用户"
+    },
+    "account": {
+      "edit": {
+        "title": "账户设定",
+        "username": "用户名",
+        "locale": "语言",
+        "theme": "主题",
+        "password": "密码",
+        "passwordconfirm": "密码（确认）",
+        "submit": "保存",
+        "cancel": "取消",
+        "success": "账户设定已更新"
+      }
+    },
+    "import": {
+      "title": "导入",
+      "submit": "提交",
+      "explain": "上传一个通过 <a href=\"https://www.google.com/takeout/\" target=\"_blank\">Google Takeout</a> 导出的 ZIP 文件或者 OPML 标准格式的文件以导入订阅",
+      "success": "导入成功，您的订阅条目会稍后显示",
+      "error": "导入过程遇到错误"
+    },
+    "export": {
+      "title": "导出",
+      "submit": "提交",
+      "explain": "导出订阅分类和条目"
+    },
+    "users": {
+      "title": "用户管理",
+      "select": "选择一位用户",
+      "edit": {
+        "newtitle": "添加新用户",
+        "submit": "提交",
+        "delete": "删除用户",
+        "deletebutton": "删除",
+        "deleteconfirm": "确认删除选定用户？",
+        "successNew": "新用户添加成功",
+        "successUpdate": "用户信息已更新"
+      }
+    }
+  },
+  "about": {
+    "newupdate": "有新的更新可用",
+    "rebuildindex": {
+      "title": "重建索引目录",
+      "message": "重建索引目录需要花费一定的时间，不过您可以继续使用本程序",
+      "button": "重建",
+      "success": "正在重建条目检索目录..."
+    },
+    "logs": {
+      "title": "查看运行记录",
+      "level": {
+        "all": "所有",
+        "fatal": "致命",
+        "error": "错误",
+        "warn": "警告",
+        "info": "信息",
+        "debug": "调试",
+        "trace": "跟踪"
+      }
+    },
+    "license": {
+      "title": "许可协议",
+      "content": "本程序授权使用 GPL 许可协议，访问 <a href=\"http://opensource.org/licenses/GPL-2.0\" target=\"_blank\">http://opensource.org/licenses/GPL-2.0</a> 了解更多"
+    },
+    "informations": {
+      "title": "更多信息",
+      "version": "运行版本:",
+      "memory": "记忆内存:",
+      "website": "官方网站:",
+      "contact": "联系邮箱:"
+    }
+  },
+  "article": {
+    "markasunread": "标记为已读"
+  },
+  "feed": {
+    "nomorearticles": "没有更多条目",
+    "nomoreunreadarticles": "没有未读条目",
+    "noarticle": "没有条目",
+    "nonewarticle": "没有新的条目",
+    "showall": "显示全部",
+    "retry": "重试"
+  },
+  "wizard": {
+    "title": "Sismics Reader 安装向导",
+    "subtitle": "开始使用之前只需要简单几个步骤",
+    "step0title": "修改管理员密码",
+    "step1title": "配置网络",
+    "step2title": "添加第一位用户",
+    "step0explain": "您应当修改默认管理员密码，特别是在您打算将阅读器部署在一个公共网络中",
+    "step1explain": "勾选 \"UPnP\" 如果您打算为阅读器设定一个新的端口",
+    "step2explain": "安装即将完成！添加第一位新用户然后开始使用 Sismics Reader",
+    "upnp": "UPnP?",
+    "upnperror": "尝试自动开启端口遇到错误，您需要手动开启",
+    "previous": "前一步",
+    "next": "下一步",
+    "keepdefaultpassword": "您确认保持默认密码？这存在一定的安全风险！",
+    "passwordconfirmerror": "两次输入的密码不一致",
+    "passwordtooshort": "密码长度过短",
+    "installationcompleted": "安装成功！"
+  },
+  "theme": {
+    "default": {
+      "less": "默认"
+    },
+    "highcontrast": {
+      "less": "高对比"
+    }
+  }
+}
