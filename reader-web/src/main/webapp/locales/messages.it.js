3374f6b098df8fb472d66cda29c6dbdbf0664515
jendib <benjamin.gam@gmail.com>
2013-06-25 19:45:35 +0200

    Italian translation by Marco Narco

---
+++ reader-web/src/main/webapp/locales/messages.it.js
@@ -0,0 +1,182 @@
+{
+  "app": "Sismics Reader",
+  "error": {
+    "unknown": "Impossibile accedere a Reader, si prega di riprovare",
+    "feed": "Impossibile caricare gli articoli, si prega di riprovare"
+  },
+  "login": {
+    "username": "Nome utente",
+    "password": "Password",
+    "remember": "Ricordami",
+    "submit": "Accedi",
+    "error": "Nome utente o password errata"
+  },
+  "defaultpassword": {
+    "info": "Reader non è sicuro. Per favore effettua la connessione con il seguente nome utente e la seguente password \"admin\", poi cambia la password immediatamente.",
+    "warning": "Non hai ancora cambiato la tua password di amministratore predefinita. Ora rendi sicuro il tuo server cambiando la password di amministratore.",
+    "dismiss": "Ignora"
+  },
+  "subscription": {
+    "latest": "Ultimi",
+    "unread": "Da leggere",
+    "all": "Tutti",
+    "starred": "Preferiti",
+    "subscriptions": "Abbonamenti",
+    "empty": "Nessun abbonamento",
+    "emptyunread": "Nessun abbonamento da leggere",
+    "showall": "Mostra tutto",
+    "toggleunread": "Alterna abbonamenti da leggere/tutti",
+    "add": {
+      "button": "Nuovo abbonamento",
+      "placeholder": "Incolla l'URL di un feed o di un sito",
+      "submit": "Aggiungi"
+    },
+    "edit": {
+      "button": "Modifica",
+      "placeholder": "Titolo",
+      "submit": "OK",
+      "delete": "Cancella",
+      "deleteconfirm": "Vuoi veramente cancellare il seguente abbonamento?"
+    }
+  },
+  "category": {
+    "nonesting": "Le categorie nidificate non sono permesse",
+    "empty": "Nessuna categoria",
+    "add": {
+      "placeholder": "Nome",
+      "submit": "Aggiungi",
+      "button": "Aggiungi una categoria"
+    },
+    "edit": {
+      "button": "Modifica",
+      "placeholder": "Nome",
+      "submit": "OK",
+      "delete": "Cancella",
+      "deleteconfirm": "Vuoi veramente cancellare la seguente categoria?"
+    }
+  },
+  "toolbar": {
+    "showall": "Mostra tutti",
+    "shownew": "Mostra nuovi",
+    "allread": "Seleziona tutti come letti",
+    "category": "Categoria",
+    "back": "&larr; Indietro",
+    "settings": "Preferenze",
+    "about": "Informazioni su",
+    "logout": "Esci"
+  },
+  "settings": {
+    "tabs": {
+      "account": "Account",
+      "importexport": "Importa/Esporta",
+      "users": "Utenti"
+    },
+    "account": {
+      "edit": {
+        "title": "Preferenze dell'account",
+        "username": "Nome utente",
+        "locale": "Localizzazione",
+        "theme": "Tema",
+        "password": "Password",
+        "passwordconfirm": "Password (conferma)",
+        "submit": "Salva",
+        "cancel": "Cancella",
+        "success": "Preferenze dell'account aggiornate"
+      }
+    },
+    "import": {
+      "title": "Importa",
+      "submit": "Importa",
+      "explain": "Carica un archivio ZIP <a href=\"https://www.google.com/takeout/\" target=\"_blank\">Google Takeout</a> oppure un file in formato OPML/XML per importare i tuoi feed.",
+      "success": "Importazione terminata con successo. I tuoi abbonamenti saranno visibili tra breve",
+      "error": "Errore nell'importazione"
+    },
+    "export": {
+      "title": "Esporta",
+      "submit": "Esporta",
+      "explain": "Esporta le tue categorie e i tuoi abbonamenti."
+    },
+    "users": {
+      "title": "Amministrazione utenti",
+      "select": "Seleziona un utente",
+      "edit": {
+        "newtitle": "Nuovo utente",
+        "submit": "Invia",
+        "delete": "Cancella utente",
+        "deletebutton": "Cancella",
+        "deleteconfirm": "Sei sicuro di voler eliminare il seguente utente?",
+        "successNew": "Aggiunto nuovo utente",
+        "successUpdate": "Utente aggiornato"
+      }
+    }
+  },
+  "about": {
+    "newupdate": "Disponibile un nuovo aggiornamento",
+    "rebuildindex": {
+      "title": "Ricostruisci l'indice di ricerca",
+      "message": "Ricostruire l'indice di ricerca può richiedere molto tempo, nel frattempo puoi continuare ad usare l'applicazione .",
+      "button": "Ricostruisci",
+      "success": "Ricostruisci l'indice degli articoli..."
+    },
+    "logs": {
+      "title": "Controlla il log",
+      "level": {
+        "all": "Tutto",
+        "fatal": "Fatale",
+        "error": "Errore",
+        "warn": "Avviso",
+        "info": "Informazione",
+        "debug": "Debug",
+        "trace": "Trace"
+      }
+    },
+    "license": {
+      "title": "Licenza",
+      "content": "Reader è rilasciato sotto i termini della licenza GPL. Leggi <a href=\"http://opensource.org/licenses/GPL-2.0\" target=\"_blank\">http://opensource.org/licenses/GPL-2.0</a> per maggiori informazioni."
+    },
+    "informations": {
+      "title": "Maggiori informazioni",
+      "version": "Versione:",
+      "memory": "Memoria:",
+      "website": "Sito:",
+      "contact": "Contatto:"
+    }
+  },
+  "article": {
+    "markasunread": "Segna come da leggere"
+  },
+  "feed": {
+    "nomorearticles": "Non sono presenti altri articoli",
+    "nomoreunreadarticles": "Non sono presenti articoli da leggere",
+    "noarticle": "Nessun articolo",
+    "nonewarticle": "Nessun nuovo articolo",
+    "showall": "Mostra tutti",
+    "retry": "Riprovare"
+  },
+  "wizard": {
+    "title": "Assistente di installazione di Sismics Reader",
+    "subtitle": "Solo alcuni passi prima di aggiungere il tuo primo feed.",
+    "step0title": "Cambia la password di amministratore",
+    "step1title": "Configura la rete",
+    "step2title": "Aggiungi il tuo primo utente",
+    "step0explain": "Dovresti cambiare la password predefinita di amministratore, soprattutto se pensi di usare Reader in una rete aperta.",
+    "step1explain": "Seleziona \"UPnP\" (apertura automatica) se vuoi che Reader provi ad aprire una porta sul tuo router.",
+    "step2explain": "È quasi finita! Aggiungi il tuo primo utente quindi inizia ad usare Sismics Reader.",
+    "upnp": "UPnP?",
+    "upnperror": "L'apertura automatica della porta non ha funzionato, prova ad aprirla manualmente",
+    "previous": "Precedente",
+    "next": "Successivo",
+    "keepdefaultpassword": "Sei sicuro di voler mantenere la password predefinita? Rappresenta un potenziale rischio per la sicurezza!",
+    "passwordconfirmerror": "Password e conferma password sono differenti",
+    "passwordtooshort": "Questa password è troppo corta",
+    "installationcompleted": "Complimenti, installazione completata!"
+  },
+  "theme": {
+    "default": {
+      "less": "Predefinito"
+    },
+    "highcontrast": {
+      "less": "Contrasto elevato"
+    }
+  }
+}
