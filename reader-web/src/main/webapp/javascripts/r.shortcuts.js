a11658933233314e7036b08c7bb385bca8688957
jendib <benjamin.gam@gmail.com>
2013-06-26 23:20:11 +0200

    Fix issue #6: keyboard shortcuts

---
+++ reader-web/src/main/webapp/javascripts/r.shortcuts.js
@@ -0,0 +1,64 @@
+/**
+ * Initializing shortcuts module.
+ */
+r.shortcuts.init = function() {
+  $(document).on('keydown', function(e) {
+    var activeTag = document.activeElement.tagName.toLowerCase();
+    if (activeTag == 'input' || activeTag == 'textarea') {
+      // User is typing, disable shortcuts
+    }
+    
+    console.log(e.which);
+    
+    switch (e.which) {
+    case 74: // J key: previous article
+    case 75: // K key: next article
+      var container = $('#feed-container');
+      if (container.is(':visible')) {
+        var selectedItem = container.find('.feed-item.selected');
+        var newItem = null;
+        
+        // Find the new selected item
+        if (selectedItem.length == 1) {
+          if (e.which == 74) {
+            newItem = selectedItem.prev('.feed-item');
+          } else {
+            newItem = selectedItem.next('.feed-item');
+          }
+        } else {
+          newItem = container.find('.feed-item:first');
+        }
+        
+        // Select the new item
+        if (newItem.length == 1) {
+          r.feed.scrollTop(newItem.position().top + container.scrollTop() + 1, true);
+        }
+      }
+      break;
+      
+    case 77: // M key: mark as read
+    case 83: // S key: star
+      var container = $('#feed-container');
+      if (container.is(':visible')) {
+        var selectedItem = container.find('.feed-item.selected');
+        if (selectedItem.length == 1) {
+          if (e.which == 77) {
+            var checkbox = selectedItem.find('.feed-item-unread input[type="checkbox"]');
+            console.log(checkbox.is(':checked'));
+            checkbox.prop('checked', !checkbox.is(':checked')).trigger('change');
+          } else {
+            selectedItem.find('.feed-item-star').click();
+          }
+        }
+      }
+      break;
+      
+    case 82: // R key: refresh feed
+      var refreshButton = $('#toolbar .refresh-button');
+      if (refreshButton.is(':visible')) {
+        refreshButton.trigger('click');
+      }
+      break;
+    }
+  });
+};
