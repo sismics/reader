394e37528c34466d6add02c9dee5f7a2c7fd776a
Jean-Marc Tremeaux <jm.tremeaux@gmail.com>
2016-11-26 17:03:15 +0100

    Remove Docker build/run scripts

--- run-service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker rm -f sismics_reader
-docker run \
-    -d --name=sismics_reader --restart=always \
-    --link sismics_reader_hsqldb:sismics_reader_hsqldb \
-    -v sismics_reader_data:/data \
-    -e 'VIRTUAL_HOST_SECURE=reader.sismics.com' -e 'VIRTUAL_PORT=80' \
-    sismics/reader:latest
