3733f5157436478fe9e2c41d1fdc8bbb80e01866
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2017-01-09 09:36:03 +0100

    Move Sismics private deployment scripts out of the public repo Fixes #123

--- build-demo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -f reader-distribution-docker/Dockerfile -t sismics/reader_demo .
--- run-service-demo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker rm -f sismics_reader_demo
-docker run \
-    -d --name=sismics_reader_demo --restart=always \
-    -e 'VIRTUAL_HOST_SECURE=reader-demo.sismics.com' -e 'VIRTUAL_PORT=80' \
-    sismics/reader_demo:latest
