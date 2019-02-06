2965b0b7310f145d2afaccff69cb7e9b4806fcad
Benjamin Gamard <b.gamard@sismics.com>
2019-02-06 14:57:21 +0100

    remove reporting

--- reader-android/app/src/main/java/com/sismics/reader/MainApplication.java
+++ reader-android/app/src/main/java/com/sismics/reader/MainApplication.java
@@ -6,11 +6,6 @@ import com.androidquery.callback.BitmapAjaxCallback;
 import com.sismics.reader.model.application.ApplicationContext;
 import com.sismics.reader.util.PreferenceUtil;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-import org.acra.sender.HttpSender.Method;
-import org.acra.sender.HttpSender.Type;
 import org.json.JSONObject;
 
 /**
@@ -18,19 +13,9 @@ import org.json.JSONObject;
  * 
  * @author bgamard
  */
-@ReportsCrashes(
-        httpMethod = Method.PUT,
-        reportType = Type.JSON,
-        formUri = "http://acralyzer.sismics.com/reader-report",
-        formUriBasicAuthLogin = "reporter",
-        formUriBasicAuthPassword = "jOS9ezJR",
-        mode = ReportingInteractionMode.TOAST,
-        resToastText = R.string.crash_toast_text)
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
-        ACRA.init(this);
-
         // Fetching /user/info from cache
         JSONObject json = PreferenceUtil.getCachedJson(getApplicationContext(), PreferenceUtil.PREF_CACHED_USER_INFO_JSON);
         ApplicationContext.getInstance().setUserInfo(getApplicationContext(), json);
